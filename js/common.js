2c2f9ebac224f17ea392ffd7f83e8242cfea0fb2
57358333@qq.com <jianglin1229>
2018-05-17 17:57:36 +0800

    5.17

--- js/common.js
+++ js/common.js
@@ -4,31 +4,27 @@ $(document).ready(function() {
             index = 0, //默认第一个菜单项
             //取当前URL最后一个/后面的文件名，pop方法是删除最后一个元素并返回最后一个元素
             url = location.href.split("?")[0].split("/").pop();
-        console.log(links[0].href);
-        console.log(url)
-        var links3 = $(".navbar-nav li .dropdownUl li a");
+        // var links3 = $(".navbar-nav li .dropdownUl li a");
         if (url) { //如果有取到，则进行匹配，否则默认首页（即index所指向的那个）
             for (var i = 0; i < links.length; i++) { //遍历menu中的链接地址
                 if (links[i].href.indexOf(url) != -1) {
                     index = i;
                     break;
-                }
-            }
-            for (var i = 0; i< links3.length; i++) {
-                if (links3[i].href.indexOf(url) != -1) {
+                }else if(url == 'newsDetails.html'){
                     index = 3;
                     break;
                 }
             }
+            // for (var i = 0; i< links3.length; i++) {
+            //     if (links3[i].href.indexOf(url) != -1) {
+            //         index = 3;
+            //         break;
+            //     }
+            // }
         }
-        console.log($(".navbar-nav>li"));
 
         $(".navbar-nav li").removeClass("active");
         $(".navbar-nav>li").eq(index).addClass("active");
-
-        $('.new').hover(function () {
-            $('.dropdownUl').toggle();
-        })
 
     });
     $("#footer").load("footer.html");
